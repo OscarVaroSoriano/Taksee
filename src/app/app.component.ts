8651c57e4dc584413bcd2fb49fac3f99636e63e0
OscarVaroSoriano <oscarvarosoriano@gmail.com>
2022-04-24 23:57:28 +0200

    improvements

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -30,21 +30,46 @@ export class AppComponent {
     this.crearTaxi(id, description, this.taxis);
   }
   askDataCarrera():void {
-    let idTaxi: number = Number(prompt("Introduce el id del taxi al que quieres asignar la carrera"));
     let recogida: string = String(prompt("Introduce la recogida"));//Select no button
     let destino: string = String(prompt("Introduce el destino"));
-    let hora: Date = new Date('2018-01-01T');
+    let horaRecogida: Date = new Date('2018-01-01T');
+    let horaFin: Date = new Date('2018-01-01T');
+
     //let horaRecogida: Date = new Date(prompt("Introduce la hora de recogida")); //Date input
-    this.carrerasAbsolute.push(new Carrera(recogida, destino, hora));
+    this.carrerasAbsolute.push(new Carrera(recogida, destino, horaRecogida, horaFin));
+    this.asignCarrerasToTaxis();
+  }
+  deleteCarrera(carrera: Carrera):void {
+    for(let i = 0; i < this.taxis.length; i++) {
+      for(let j = 0; j < this.taxis[i].carrerasProgramadas.length; j++) {
+        if(this.taxis[i].carrerasProgramadas[j] == carrera) {
+          this.taxis[i].carrerasProgramadas.splice(j, 1);
+        }
+      }
+    }
   }
   showDataTaxi():void {
     for (let taxi of this.taxis) {
       console.log(taxi);
     }
   }
+  cuadraEnHorario(carrera: Carrera, taxi: Taxi):boolean {
+    return true;
+    for(let i = 0; i < taxi.carrerasProgramadas.length; i++) {
+        if(carrera.horaRecogida>= taxi.carrerasProgramadas[i].horaRecogida && carrera.horaRecogida <= taxi.carrerasProgramadas[i].horaFin) {
+          return false;
+        }
+        else if(carrera.horaFin >= taxi.carrerasProgramadas[i].horaRecogida && carrera.horaFin <= taxi.carrerasProgramadas[i].horaFin) {
+          return false;
+        }
+      }
+    return true;
+  }
 
   asignCarrerasToTaxis():void {
+    //Recorro la lista de todas las carreras
     for(let i = 0; i < this.carrerasAbsolute.length; i++) {
+      //Recorro la lista de todos los taxis
       for(let j = 0; j < this.taxis.length; j++) {
         // Si es el primer registro le hago un add al primer taxi
         if(i == 0 && j == 0) {
@@ -52,7 +77,12 @@ export class AppComponent {
         }
         // Tengo que controlar todas las horas para que no coincidan los taxis
         else{
-          
+          //Check if the taxi is free at that time
+          if(this.cuadraEnHorario(this.carrerasAbsolute[i], this.taxis[j]))
+          {
+            //Si no coincide le hago un add al taxi
+            this.taxis[j].carrerasProgramadas.push(this.carrerasAbsolute[i]);
+          }
         }
       }  
     }
--- src/app/carrera.ts
+++ src/app/carrera.ts
@@ -2,11 +2,13 @@ export class Carrera{
     recogida: string;
     destino: string;
     horaRecogida: Date;
+    horaFin: Date;
 
-    constructor(recogida: string, destino: string, horaRecogida: Date){
+    constructor(recogida: string, destino: string, horaRecogida: Date, horaFin: Date){
         this.recogida = recogida;
         this.destino = destino;
         this.horaRecogida = horaRecogida;
+        this.horaFin = horaFin;
     }
 
 }
